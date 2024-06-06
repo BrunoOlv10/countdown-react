c73807afdd66614b22e685f24101166ad6b2429e
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2024-06-05 22:53:28 -0300

    add datas

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,19 @@
+import PropTypes from 'prop-types';
 import './Counter.css'
 
+
 const Counter = ({title, number}) => {
-  return (
-    <div className="counter">
+    return (
+        <div className="counter">
       <p className="counter-number">{number}</p>
       <h3 className="counter-text">{title}</h3>
     </div>
   )
 }
+
+Counter.propTypes = {
+    title: PropTypes.string,
+    number: PropTypes.number,
+};
 
 export default Counter
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,15 @@
+import PropTypes from 'prop-types';
 import './Title.css'
+
 
 const Title = ({title}) => {
   return (
     <h1 className="title">{title}</h1>
   )
 }
+
+Title.propTypes = {
+  title: PropTypes.string,
+};
 
 export default Title
